ff35b9e31151eef31dd0ed946849b9c4890174f3
Walcker Gomes <walckergomes@gmail.com>
2022-05-25 14:03:20 -0300

    Funcionanado completa. Duas interfaces.

--- src/br/ufrn/imd/MiltonNascimento.java
+++ src/br/ufrn/imd/MiltonNascimento.java
@@ -1,8 +1,12 @@
 package br.ufrn.imd;
 
-public class MiltonNascimento implements Cantor{
+public class MiltonNascimento implements Cantor, Dancarino{
 
     public String cantar(){
         return "Amigo é coisa para se guardar...";
+    }
+
+    public String dancar(){
+        return "Dançando com Milton...";
     }
 }
--- src/br/ufrn/imd/Principal.java
+++ src/br/ufrn/imd/Principal.java
@@ -2,9 +2,21 @@ package br.ufrn.imd;
 
 public class Principal {
 
+    public void facoDancar(Dancarino dancarino){
+        System.out.println(dancarino.dancar());
+    }
+
+    public void facoCantar(Cantor cantor){
+        System.out.println(cantor.cantar());
+    }
+
     public static void main(String[] args) {
-        Cantor milton = new MiltonNascimento();
-        System.out.println(milton.cantar());
+
+        Principal principal = new Principal();
+
+        MiltonNascimento milton = new MiltonNascimento();
+        principal.facoCantar(milton);
+        principal.facoDancar(milton);
     }
 
 }
